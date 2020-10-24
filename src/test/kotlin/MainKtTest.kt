978776f768b296856282d261492de3fff1875026
Peter Konovodov <p_konovodov@mail.ru>
2020-10-24 12:22:41 +0500

    3, добавлено Jacoco, оптимизированы тесты

--- src/test/kotlin/MainKtTest.kt
+++ src/test/kotlin/MainKtTest.kt
@@ -12,38 +12,26 @@ class MainKtTest {
     @Test
     fun getCommission() {
         val expected = 0
-        val transferAmount = 1000
-        val result = getCommission(transferAmount = transferAmount)
-        assertEquals(expected, result)
+        assertEquals(expected, getCommission(transferAmount = 1000))
     }
 
     @Test
     fun getCommission_MasterCard() {
-        val expected = 0
-        val transferAmount = 1000
-        val result = getMasterCardCommission(transferAmount = transferAmount)
-        assertEquals(expected, result)
-    }
-
-    @Test
-    fun getCommission_MasterCard_over75000() {
-        val expected = 26
-        val cardType = "MasterCard"
-        val monthSum = 75001
-        val transferAmount = 1000
-        val result = getCommission(cardType, monthSum, transferAmount)
-        assertEquals(expected, result)
+        var expected = 0
+        assertEquals(expected, getMasterCardCommission(transferAmount = 1000))
+        expected = 26
+        assertEquals(expected, getMasterCardCommission(monthSum = 75001, transferAmount = 1000))
     }
 
 
     @Test
     fun getCommission_Visa() {
-        val expected = 35
         val cardType = "VISA"
         val monthSum = 75000
-        val transferAmount = 1000
-        val result = getCommission(cardType, monthSum, transferAmount)
-        assertEquals(expected, result)
+        var expected = 35
+        assertEquals(expected, getCommission(cardType, monthSum, transferAmount = 1000))
+        expected = 75
+        assertEquals(expected, getCommission(cardType, monthSum, transferAmount = 10000))
     }
 
 }
